91b55f793657d8a8d1551f7a4cb5fddc98e012a5
priya upadhyay <upad.priya@gmail.com>
2019-06-30 23:53:38 +0530

    Update GreeterController.java

--- src/main/java/hello/GreeterController.java
+++ src/main/java/hello/GreeterController.java
@@ -1,4 +1,7 @@
 package main.java.hello;
+import org.igfay.jfig.JFig;
+import org.igfay.jfig.JFigException;
+import org.igfay.jfig.JFigIF;
 
 import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,10 +10,17 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class GreeterController {
-
+	private static final JFigIF template;
+	
+	try {
+		 template = JFig.initialize();
+	} catch(JFigException e){
+		e.printStackTrace();
+	}
    
+
     @RequestMapping("/greeter")
     public Greeter greeter() {
-        return new Greeter();
+        return new Greeter(template);
     }
 }
