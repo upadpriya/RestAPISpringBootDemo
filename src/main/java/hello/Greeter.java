c0bc93337f37fea6e012f34cdcfcd2b7d873bf4e
priya upadhyay <upad.priya@gmail.com>
2019-06-30 23:54:10 +0530

    Update Greeter.java

--- src/main/java/hello/Greeter.java
+++ src/main/java/hello/Greeter.java
@@ -1,19 +1,20 @@
 package main.java.hello;
 import org.igfay.jfig.JFig;
 import org.igfay.jfig.JFigException;
-import org.json.JSONObject;
-import org.json.XML;
+import org.igfay.jfig.JFigIF;
 
 
 public class Greeter {
 
-    public Greeter() {
-    	   try {
-   			JFig.initialize();
-   		} catch (JFigException e) {
-   			// TODO Auto-generated catch block
-   			e.printStackTrace();
-   		}
+	//private final long id;
+    private final JFigIF content;
+
+    public Greeter( JFigIF content) {
+        
+        this.content = content;
     }
+
+    
+	
 	
 }
